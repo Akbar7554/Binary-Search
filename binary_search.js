b66778723cb01991faa3eb71c10af574b87f64e5
Akbar Hossain <hakbar431@gmail.com>
2023-12-20 08:24:12 +0600

    binary search

--- binary_search.js
+++ binary_search.js
@@ -1,21 +1,24 @@
-function binarySearch(arr, target) {
-    let left = 0;
-    let right = arr.length - 1;
-
+function binarySearch(myArray, n, t) { //n is array number of elements and t is target which we find in an array
+    let left, right, mid
+    left = 0
+    right = n - 1
     while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-
-        if (arr[mid] === target) return mid;
-
-        if (target < arr[mid]) {
-            right = mid - 1;
-        } else {
-            left = mid + 1;
+        mid = Math.floor((left + right) / 2)
+        if (myArray[mid] == t) {
+            return mid
+        }
+        if (myArray[mid] < t) {
+            left = mid + 1
+        }
+        else {
+            right = mid - 1
         }
     }
-
-    return "Target Not Found"
-    // could also return -1, false, undefined, etc
+    return - 1
 }
 
-console.log(binarySearch([3,6,4,6,6,73,], 3));
+let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+let n = 19
+let t = 5
+const answer = binarySearch(myArray, n, t)
+console.log("Answer is :",answer);
